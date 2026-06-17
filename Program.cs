40fcbe8e0d0b2a34f8d119eee2e1a56b1928e4c0
Voixed <167946983+Voixed@users.noreply.github.com>
2026-06-17 17:58:07 -0600

    New code has been added that checks if there is a winner

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         private int rows;
         private int cols;
 
-        public GameBoard(int rows = 7, int cols = 3)
+        public GameBoard(int rows = 6, int cols = 7)
         {
             this.rows = rows;
             this.cols = cols;
@@ -20,12 +20,94 @@
                 for (int j = 0; j < cols; j++)
                     board[i, j] = "";
         }
+
+        public string HasWinner()
+        {
+            string result = null;
+
+            // Row check
+            for (int i = 0; i < rows; i++)
+            {
+                if (board[i, 0] != "" &&
+                    board[i, 0] == board[i, 1] &&
+                    board[i, 1] == board[i, 2] &&
+                    board[i, 2] == board[i,3])
+                {
+                    result = board[i, 0];
+                    break;
+                }
+            }
+
+            // Diagonal check (top left - bottom right)
+            for (int i = 0; i < rows - 3; i++)
+            {
+                for (int j = 0; j < cols - 3; j++)
+                {
+                    if (board[i, j] != "" &&
+                        board[i, j] == board[i + 1, j + 1] &&
+                        board[i + 1, j + 1] == board[i + 2, j + 2] &&
+                        board[i + 2, j + 2] == board[i + 3, j + 3])
+                    {
+                        result = board[i, j];
+                        break;
+                    }
+                }
+            }
+
+            // Diagonal check (top right - bottom left)
+            for (int i = 0; i < rows - 3; i++)
+            {
+                for (int j = 3; j < cols; j++)
+                {
+                    if (board[i, j] != "" &&
+                        board[i, j] == board[i + 1, j - 1] &&
+                        board[i + 1, j - 1] == board[i + 2, j - 2] &&
+                        board[i + 2, j - 2] == board[i + 3, j - 3])
+                    {
+                        result = board[i, j];
+                        break;
+                    }
+                }
+            }
+
+            // Column check
+            // Column check
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows - 3; j++)
+                {
+                    if (board[j, i] != "" &&
+                        board[j, i] == board[j + 1, i] &&
+                        board[j + 1, i] == board[j + 2, i] &&
+                        board[j + 2, i] == board[j + 3, i])
+                    {
+                        result = board[j, i];
+                        break;
+                    }
+                }
+            }
+            //Tie Check
+            if (result == null && IsBoardFull())
+                result = "tie";
+
+            return result;
+        }
+
+        private bool IsBoardFull()
+        {
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    if (board[i, j] == "") return false;
+
+            return true;
+        }
+    }
         internal class Program
         {
             static void Main(string[] args)
             {
-                Console.WriteLine("Hello, World!");
+
             }
         }
-    }
 }
+
