b69ca5a0c44f8ccf574e0e2004f50f2078ca9d03
Voixed <167946983+Voixed@users.noreply.github.com>
2026-06-17 14:54:43 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FinalProjectConnect4
+{
+    public class GameBoard
+    {
+        private string[,] board;
+        private int rows;
+        private int cols;
+
+        public GameBoard(int rows = 7, int cols = 3)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            board = new string[rows, cols];
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    board[i, j] = "";
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
